e0acdcb130d5f97aee10e332ca8811768416b724
Kor-Lazyman <taekwonv0310@gmail.com>
2024-05-16 18:22:18 +0900

    Path_Change

--- src/InventoryMgtEnv.py
+++ src/InventoryMgtEnv.py
@@ -9,7 +9,6 @@ from log_RL import *
 import random
 from torch.utils.tensorboard import SummaryWriter
 
-
 class GymInterface(gym.Env):
     def __init__(self):
         self.shortages = 0
@@ -21,28 +20,12 @@ class GymInterface(gym.Env):
             # Define action space
             self.action_space = spaces.Discrete(len(ACTION_SPACE))
             # Define observation space:
-            # self.observation_space = spaces.Box(low=0, high=INVEN_LEVEL_MAX, shape=(len(I),), dtype=int)
             os = []
             for _ in range(len(I)):
                 os.append(INVEN_LEVEL_MAX+1)
                 os.append(DEMAND_QTY_MAX+1+DELTA_MIN)
                 os.append(DEMAND_QTY_MAX+1)
             self.observation_space = spaces.MultiDiscrete(os)
-        elif RL_ALGORITHM == "DDPG":
-            # self.action_space = spaces.Box(low=0, high=len(ACTION_SPACE), shape=(1,), dtype=np.float32)
-            actionSpace_low = []
-            actionSpace_high = []
-            for _ in range(len(I)):
-                if I[_]["TYPE"] == "Material":
-                    actionSpace_low.append(0)
-                    actionSpace_high.append(len(ACTION_SPACE))
-            self.action_space = spaces.Box(low=np.array(actionSpace_low), high=np.array(
-                actionSpace_high), dtype=np.float32)
-            # self.observation_space = spaces.Box(low=0, high=INVEN_LEVEL_MAX, shape=(len(I),), dtype=np.float32)
-            os = [101 for _ in range(len(I))]
-            os.append(DEMAND_QTY_MAX + 1)
-            os.append(DEMAND_QTY_MAX+1)
-            self.observation_space = spaces.Discrete(os)
         elif RL_ALGORITHM == "PPO":
             # Define action space
             actionSpace = []
@@ -50,27 +33,25 @@ class GymInterface(gym.Env):
                 if I[i]["TYPE"] == "Material":
                     actionSpace.append(len(ACTION_SPACE))
             self.action_space = spaces.MultiDiscrete(actionSpace)
-            '''
-            # Define observation space:
-            for i in range(len(I)):
-                os.append(INVEN_LEVEL_MAX+1)
-                os.append(INVEN_LEVEL_MAX+1+DELTA_MIN)
-            # os.append(DEMAND_QTY_MAX + 1+EXPECTED_PRODUCT_MAX)
-            os.append(INVEN_LEVEL_MAX + DEMAND_QTY_MAX+1)
-            self.observation_space = spaces.MultiDiscrete(os)
-            print(os) 
-            
-            # Define the size of observation space:
-            os = [upper - lower + 1 for lower, upper in STATE_RANGES]
-            '''
+         
             os=[102 for _ in range(len(I)*2+1)]
             self.observation_space = spaces.MultiDiscrete(os)
             print(os)
 
+        elif RL_ALGORITHM == "PPO":
+            # Define action space
+            actionSpace = []
+            for i in range(len(I)):
+                if I[i]["TYPE"] == "Material":
+                    actionSpace.append(len(ACTION_SPACE))
+            self.action_space = spaces.MultiDiscrete(actionSpace)
+         
+            os=[102 for _ in range(len(I)*2+1)]
+            self.observation_space = spaces.MultiDiscrete(os)
+            print(os)
         self.total_reward_over_episode = []
         self.total_reward = 0
         self.num_episode = 1
-
     def reset(self):
         # Initialize the simulation environment
         print("\nEpisode: ", self.num_episode)
@@ -90,14 +71,12 @@ class GymInterface(gym.Env):
         # Update the action of the agent
         if RL_ALGORITHM == "DQN":
             I[1]["LOT_SIZE_ORDER"] = action
-            # I[0]["DEMAND_QUANTITY"] = random.randint(DEMAND_QTY_MIN, DEMAND_QTY_MAX)
-            # I[0]["DUE_DATE"] = random.randint(DUE_DATE_MIN, DUE_DATE_MAX)
+
 
         elif RL_ALGORITHM == "DDPG":
             i = 0
             for _ in range(len(I)):
                 if I[_]["TYPE"] == "Material":
-                    # I[_]["LOT_SIZE_ORDER"] = int(round(action[i]))
                     I[_]["LOT_SIZE_ORDER"] = action[i]
                     i += 1
         elif RL_ALGORITHM == "PPO":
@@ -122,16 +101,16 @@ class GymInterface(gym.Env):
         next_state_corr = self.correct_state_for_SB3(next_state_real)
         # Calculate the total cost of the day
         env.Cost.update_cost_log(self.inventoryList)
-        if VALIDATION:
+        if PRINT_SIM:
             cost=dict(DAILY_COST_REPORT)
         env.Cost.clear_cost()
-        # reward = -COST_LOG[-1]-next_state[-1]*I[0]["SHORTAGE_COST_PRO"]
+
         reward = -COST_LOG[-1]
         self.total_reward += reward
         self.shortages += self.sales.num_shortages
         self.sales.num_shortages = 0
         
-        if VALIDATION:
+        if PRINT_SIM:
             # Print the simulation log every 24 hours (1 day)
             print(f"\nDay {(self.simpy_env.now+1) // 24}:")
             if RL_ALGORITHM == "DQN":
@@ -182,23 +161,9 @@ class GymInterface(gym.Env):
         temp.append(I[0]["DEMAND_QUANTITY"]-DAILY_REPORTS[-1][6])#append remaining demand
         STATE_ACTION_REPORT_REAL.append(temp)
         return STATE_ACTION_REPORT_REAL[-1]
+    
     #Min-Max Normalization    
     def correct_state_for_SB3(self,state):
-        
-        '''
-        for inven in self.inventoryList:
-        
-            # Function to capture the current state of the inventory
-            state.append(
-                inven.daily_inven_report[4]-inven.daily_inven_report[5]+DELTA_MIN)
-            state.append(inven.daily_inven_report[6])
-            # Reset Report
-            
-        # state.append(I[0]['DEMAND_QUANTITY'] - self.inventoryList[0].on_hand_inventory+EXPECTED_PRODUCT_MAX)
-        
-        state.append(I[0]['DEMAND_QUANTITY'] -
-                     self.inventoryList[0].on_hand_inventory+INVEN_LEVEL_MAX)
-        '''
         #Update STATE_ACTION_REPORT_CORRECTION.append(state_corrected)
         state_corrected=[]
         for id in range(len(I)):
@@ -210,23 +175,6 @@ class GymInterface(gym.Env):
     
     def render(self, mode='human'):
         pass
-        # if EPISODES == 1:
-        #     self.visualize()
-        # else:
-        #     if OPTIMIZE_HYPERPARAMETERS:
-        #         pass
-        #     else:
-        #         self.visualize()
-
-        #     # Total rewards over episodes
-        #     fig = plt.figure(1, figsize=(14, 5))
-        #     ax = fig.add_subplot(1, 1, 1)
-        #     plt.plot(self.total_reward_over_episode, lw=4,
-        #              marker='o', markersize=10)
-        #     ax.tick_params(axis='both', which='major', labelsize=15)
-        #     plt.xlabel('Episodes', size=20)
-        #     plt.ylabel('Total Rewards', size=20)
-        #     plt.show()
 
     def close(self):
         # 필요한 경우, 여기서 리소스를 정리
--- src/config_SimPy.py
+++ src/config_SimPy.py
@@ -59,7 +59,7 @@ P = {0: {"ID": 0, "PRODUCTION_RATE": 2, "INPUT_TYPE_LIST": [I[1]], "QNTY_FOR_INP
 INVEN_LEVEL_MIN = 0
 INVEN_LEVEL_MAX = 50  # Capacity limit of the inventory [units]
 DEMAND_QTY_MIN = 10
-DEMAND_QTY_MAX = 16
+DEMAND_QTY_MAX = 14
 
 # Simulation
 SIM_TIME = 100  # 200 [days] per episode
@@ -81,7 +81,7 @@ ORDER_QTY = 2
 REORDER_LEVEL = 0
 
 # Print logs
-VALIDATION = False
+PRINT_SIM=True
 # PRINT_LOG_TIMESTEP = True
 # PRINT_LOG_DAILY_REPORT = True
 
@@ -89,5 +89,5 @@ VALIDATION = False
 # If False, the total cost is calculated based on the inventory level for every 24 hours.
 # Otherwise, the total cost is accumulated every hour.
 HOURLY_COST_MODEL = True
-VISUALIAZTION = [0, 0, 0]  # PRINT RAW_MATERIAL, WIP, PRODUCT
+VISUALIAZTION = [1, 0, 1]  # PRINT RAW_MATERIAL, WIP, PRODUCT
 TIME_CORRECTION = 0.0001
