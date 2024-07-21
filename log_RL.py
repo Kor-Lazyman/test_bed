210a5a63bdb0057704d69f1347d29135f4ef8610
Kor-Lazyman <51987000+Kor-Lazyman@users.noreply.github.com>
2024-07-21 22:25:42 +0900

    Delete log_RL.py

--- log_RL.py
+++
@@ -1,4 +0,0 @@
-# Log daily repots: Inventory level for each item; daily change for each item; Remaining demand (demand - product level)
-STATE_ACTION_REPORT_CORRECTION = []#Corrected State
-STATE_ACTION_REPORT_REAL=[]#Real State
-COST_RATIO_HISTORY=[]
